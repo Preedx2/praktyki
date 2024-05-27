f93854cad3e31d122df9bff2e1156474664ede85
Preedx2 <patryk.hernas@student.po.edu.pl>
2024-05-27 23:49:14 +0200

    added documentation to database.py

--- praktyki-main-app/source/database.py
+++ praktyki-main-app/source/database.py
@@ -5,10 +5,16 @@ from bson.json_util import dumps
 
 import source.collections.users as users
 
+
 class Database:
-
+    """
+    Class representing database connection
+    """
     def __init__(self):
-
+        """
+           On creation object communicates with remote database using informations contained in mongodb-login file.
+           The file should contain name of the mongodb cluster, login and password, each on separate line
+        """
         with open("mongodb-login", 'r') as file:
             _cluster = file.readline()[:-1]
             _login = file.readline()[:-1]
@@ -18,7 +24,11 @@ class Database:
         self.client = MongoClient(uri, server_api=ServerApi('1'))
         self.database = self.client.get_database("praktyki_app_db")
 
-    def __del__(self):
+    def __del__(self) -> None:
+        """
+        On deletion object closes connection to database
+        @return: None
+        """
         if hasattr(self, 'client'):
             self.client.close()
 
@@ -39,13 +49,33 @@ class Database:
         users.add_random_user(self.database)
 
     def search(self, collection_name: str, query: dict) -> dict:
+        """
+        searches for one instance of query in collection collection_name
+        @param collection_name: name of the collection to search, str
+        @param query: query to search - dict consisting of key: value pairs
+        @return: dict with all attributes of an object satisfying query result,
+                or empty dictionary if query was unsuccessful
+        """
         collection = self.database.get_collection(collection_name)
         return collection.find_one(query)
 
     def find_one_and_update(self, collection_name: str, query: dict, update: dict) -> dict:
+        """
+        searches for one instance of query in collection and updates its values
+        @param collection_name: name of the collection to search
+        @param query: query to search - dict consisting of key: value pairs
+        @param update: values to update - consisting of key: value pairs
+        @return: dict with updated object or empty dictionary if query was unsuccessful
+        """
         collection = self.database.get_collection(collection_name)
         return collection.find_one_and_update(query, update)
 
-    def insert(self, collection_name: str, query: dict) -> None:
+    def insert(self, collection_name: str, object_dict: dict) -> None:
+        """
+        inserts one object into specified collection
+        @param collection_name: name of the collection you insert object into
+        @param object_dict: object to insert, represented as key: value pairs
+        @return: None
+        """
         collection = self.database.get_collection(collection_name)
-        collection.insert_one(query)
+        collection.insert_one(object_dict)
